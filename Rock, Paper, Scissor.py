22c0d403cf9fa804b819d5d573baf07d71f07651
LBJ-Nor <80926104+LBJ-Nor@users.noreply.github.com>
2021-07-16 03:32:32 +0200

    main

--- Rock, Paper, Scissor.py
+++ Rock, Paper, Scissor.py
@@ -1,12 +1,12 @@
-
 import random
+
 print("""WELCOME WELCOME, This is rock, paper, scissors please select a letter to play!
 type help for input options""")
 choices = ["R", "P", "S"]
 w_outcome = ["RS", "PR", "SP"]
 player_score = 0
 npc_score = 0
-help = """
+help_text = """
 R - Rock
 P - Paper
 S - Scissors
@@ -15,40 +15,32 @@ type quit to end
 
 
 def game_rules():
-    for choice in choices:
-        if player_choice.upper() + npc_choice in w_outcome:
-            global player_score
-            player_score += 1
-            print("You chose:", player_choice.upper(), "Computer chose:", npc_choice)
-            print("You win")
-            global npc_score
-            print("Player score:", player_score, "NPC score:", npc_score)
-            return
-        elif npc_choice == player_choice.upper():
-            print("You chose:", player_choice.upper(), "Computer chose:", npc_choice)
-            print("It`s a draw")
-            print("Player score:", player_score, "NPC score:", npc_score)
-            return
-        elif npc_choice + player_choice.upper() in w_outcome:
-            npc_score += 1
-            print("You chose:", player_choice.upper(), "Computer chose:", npc_choice)
-            print("You lose")
-            print("Player score:", player_score, "NPC score:", npc_score)
-            return
-        if player_choice.lower() == "help":
-            print(help)
-            return
-        elif player_choice.lower() == "quit":
-            break
-        else:
-            if player_choice not in choices:
-                print("Please enter R, P, or S for Rock, Paper or Scissor")
-            return
+    global player_score
+    global npc_score
+    if player_choice + npc_choice in w_outcome:
+        player_score += 1
+        print("You chose:", player_choice, "Computer chose:", npc_choice)
+        print("You win")
+        print("Player score:", player_score, "NPC score:", npc_score)
+    elif npc_choice == player_choice:
+        print("You chose:", player_choice, "Computer chose:", npc_choice)
+        print("It`s a draw")
+        print("Player score:", player_score, "NPC score:", npc_score)
+    elif npc_choice + player_choice in w_outcome:
+        npc_score += 1
+        print("You chose:", player_choice, "Computer chose:", npc_choice)
+        print("You lose")
+        print("Player score:", player_score, "NPC score:", npc_score)
+    elif player_choice not in choices:
+        print("Please enter R, P, or S for Rock, Paper or Scissor")
+
 
 while True:
-    player_choice = input("R, P, S?: ")
+    player_choice = input("R, P, S?: ").upper()
     npc_choice = (random.choice(choices))
     game_rules()
-    if player_choice.lower() == "quit":
+    if player_choice == "HELP":
+        print(help_text)
+    if player_choice == "QUIT":
         print("Thank you for playing.")
         break
